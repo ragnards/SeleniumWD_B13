225cbd38701789f98d9e9be0378efa9682bccdd6
ragnards <mail>
2020-03-19 19:37:25 +0300

    добавлен тест на логин

---
+++ src/main/java/ru/ragnards/selenium/task3/LoginTest.java
@@ -0,0 +1,33 @@
+package ru.ragnards.selenium.task3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testLogin() {
+        driver.navigate().to("http://localhost:8880/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
